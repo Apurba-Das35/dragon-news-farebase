99529fe693aa4b8423402d9d185ca351cca026ce
Apurbo-Das35 <apurbadas3511@gmail.com>
2026-01-05 17:13:47 +0600

    avarage added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,10 +2,7 @@ import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
 
-    const [user, setUser] = useState({
-        name: "Apurbo Das",
-        email: "apurbodas@gmail.com",
-    });
+    const [user, setUser] = useState(null);
 
     const authData = {
         user,
